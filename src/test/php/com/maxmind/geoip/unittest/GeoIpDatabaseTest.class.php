11c9b33beda3bab38a26eabfd79ca4aefaa41b11
Timm Friebe <thekid@thekid.de>
2015-02-10 23:44:07 +0100

    Add test data from MaxMind. Fixes #2
    See https://github.com/maxmind/MaxMind-DB/blob/master/source-data/GeoIP2-City-Test.json
    Be nice on everyone's bandwith and commit locally

--- src/test/php/com/maxmind/geoip/unittest/GeoIpDatabaseTest.class.php
+++ src/test/php/com/maxmind/geoip/unittest/GeoIpDatabaseTest.class.php
@@ -1,47 +1,52 @@
 <?php namespace com\maxmind\geoip\unittest;
 
 use com\maxmind\geoip\GeoIpDatabase;
-use io\streams\FileInputStream;
 use lang\ClassLoader;
 use lang\IllegalArgumentException;
-use unittest\PrerequisitesNotMetError;
 
 class GeoIpDatabaseTest extends \unittest\TestCase {
-  const DATABASE = 'GeoLite2-City.mmdb';
+  const DATABASE = 'GeoIP2-City-Test.mmdb';
+  private $fixture;
 
+  /**
+   * Sets up test
+   */
   public function setUp() {
-    $this->loader= ClassLoader::getDefault();
-    if (!$this->loader->providesResource(self::DATABASE)) {
-      throw new PrerequisitesNotMetError('Download database first', null, [self::DATABASE]);
-    }
+    $this->fixture= ClassLoader::getDefault()->getResourceAsStream(self::DATABASE)->in();
   }
 
   #[@test]
   public function open() {
-    GeoIpDatabase::open($this->loader->getResourceAsStream(self::DATABASE)->in());
+    GeoIpDatabase::open($this->fixture);
   }
 
   #[@test]
   public function lookup_v4_localhost() {
-    $reader= GeoIpDatabase::open($this->loader->getResourceAsStream(self::DATABASE)->in());
+    $reader= GeoIpDatabase::open($this->fixture);
     $this->assertEquals(null, $reader->lookup('127.0.0.1'));
   }
 
-  #[@test, @values(['92.203.53.176', '8.8.8.8'])]
+  #[@test, @values(['89.160.20.128', '216.160.83.56'])]
   public function lookup_v4($addr) {
-    $reader= GeoIpDatabase::open($this->loader->getResourceAsStream(self::DATABASE)->in());
+    $reader= GeoIpDatabase::open($this->fixture);
     $this->assertNotEquals(null, $reader->lookup($addr));
   }
 
   #[@test]
   public function lookup_v6_localhost() {
-    $reader= GeoIpDatabase::open($this->loader->getResourceAsStream(self::DATABASE)->in());
+    $reader= GeoIpDatabase::open($this->fixture);
     $this->assertEquals(null, $reader->lookup('::1'));
+  }
+
+  #[@test, @values(['2001:256::', '2a02:da80::'])]
+  public function lookup_v6($addr) {
+    $reader= GeoIpDatabase::open($this->fixture);
+    $this->assertNotEquals(null, $reader->lookup($addr));
   }
 
   #[@test, @expect(IllegalArgumentException::class), @values([null, '', 'not.an.ip', '::not-v6'])]
   public function lookup_raises_an_exception_when_input_is_not_an_ip_address($value) {
-    $reader= GeoIpDatabase::open($this->loader->getResourceAsStream(self::DATABASE)->in());
+    $reader= GeoIpDatabase::open($this->fixture);
     $reader->lookup($value);
   }
 }
